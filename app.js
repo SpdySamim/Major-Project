baf92341d1d08ca4092652501ff674e097a58596
Samim <tonystarkvsthor789@gmail.com>
2024-01-14 21:36:50 +0530

    checking

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ if(process.env.NODE_ENV !="production"){
     require('dotenv').config();
 }
 console.log(process.env.SECRET)
+console.log(process.env.ATLASDB_URL)
 //express setup
 const express=require("express");
 const app=express();
